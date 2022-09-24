5ba7ffb6f58855889d53a7d1c98a564deacad10a
Joseph-Jnr <josephnwobodo1@gmail.com>
2022-09-24 13:47:37 +0100

    Camera permissions js

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,3 +94,27 @@ $('.app_btn').click(function () {
 $('.chat-bot-btn').click(function(){
     $('.chat-bot-frame, .msg-count').toggle()
 })
+
+//how to open camera using javascript?
+var video = document.createElement('video');
+video.setAttribute('playsinline', '');
+video.setAttribute('autoplay', '');
+video.setAttribute('muted', '');
+video.style.width = '200px';
+video.style.height = '200px';
+
+/* Setting up the constraint */
+var facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
+var constraints = {
+  audio: false,
+  video: {
+   facingMode: facingMode
+  }
+};
+
+/* Stream it to video element */
+navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+  video.srcObject = stream;
+});
+
+
